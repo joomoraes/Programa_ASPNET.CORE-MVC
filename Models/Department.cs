e2eb4df1aae1853878c0a9e8e383e869be996a07
João Alves <joao.alves@liberali.local>
2021-04-01 14:00:33 -0400

    Desenvolvimento de outras entiddes migrations

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SalesWebMVC.Models
 {
@@ -6,5 +9,19 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sallers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sallers.Add(seller);
+        }
     }
 }
